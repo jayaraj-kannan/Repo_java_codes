453b8d443b20d223b37db2cc859cf127f4dbfb62
jkn15 <jkn15@3ds.com>
2023-04-02 19:23:36 +0530

    deleted few files

--- java_practice/p_1/src/FlippingBits.java
+++
@@ -1,9 +0,0 @@
-
-public class FlippingBits {
-	public static void main(String[] args) {
-		Long n=(long) 14;
-	System.out.println((long)Math.pow(2,32) + (~n));
-	Long THE_FLIPPING = 4294967295L;
-	System.out.println(n ^ THE_FLIPPING);
-	}
-}
